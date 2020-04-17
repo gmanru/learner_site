537bbb8e220619f7dddb1c2ae6cce04bca2210f6
Gman <mrgman11@yandex.ru>
2020-04-17 20:33:35 +0300

    first start. Added 2 models(applications): teacher_part, student

---
+++ learner_site/teacher_part/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
+
+
+class Course(models.Model):
+    '''TTR_L_1 = 1
+    TTR_1_5 = 2
+    TTR_10 = 3
+    TTR_12 = 4
+    TTR_24 = 5'''
+
+    TIME_TO_LEARN = (
+        (1, '< 1 month'),
+        (2, '5 month'),
+        (3, '10 month'),
+        (4, '1 year'),
+        (5, '> 2 year'),
+    )
+
+    title = models.CharField(max_length=254)
+    text = models.TextField()
+    time_to_learn = models.IntegerField(choices=TIME_TO_LEARN)
+    ts_last_changed = models.DateTimeField(auto_now=True)
+    ts_created = models.DateTimeField(auto_now_add=True)
+
+    teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
