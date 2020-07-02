095d78450b9b1a8073d32ef85ad4d65e1bd88ff3
Gman <mrgman11@yandex.ru>
2020-07-02 22:03:42 +0300

    added react, render list of teachers and their emails

--- learner_site/frontend/src/index.js
+++ learner_site/frontend/src/index.js
@@ -1,9 +1,2 @@
-const axios = require("axios");
+import App from "./components/App"
 
-
-axios.get("http://127.0.0.1:8000/people/api/student/")
-  .then(response => console.log("response", response.data))
-
-fetch('http://127.0.0.1:8000/course/api/')
-  .then(response => response.json())
-  .then(data => console.log(data));
